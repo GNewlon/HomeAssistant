6c17fdc459aa74db1132e982aa568b8f0806c4af
root <newlon@outlook.com>
2022-11-22 20:48:44 -0500

    112222.20:48:44

--- pushupdates.sh
+++
@@ -1,6 +0,0 @@
-git config --global --add safe.directory /config
-git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
-git config --global user.email "newlon@outlook.com"
-git add .
-git commit -m "`date +'%m%d%y.%H:%M:%S'`"
-git push -u origin master
